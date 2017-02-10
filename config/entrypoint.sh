ac66e0a97130a4ffc382056bc3c3e482a0f50c05
ALLAOUI Local <allaoui.amine@gmail.com>
2017-02-10 16:28:28 +0000

    all

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -4,6 +4,6 @@
 #composer update 
 chown -R apache:apache /var/www/html 
 chmod -R 550 /var/www/html/ 
-chmod -R 750 /var/www/html/app/storage 
-chmod -R 750 /var/www/html/app/bootstrap/cache
+chmod -R 770 /var/www/html/app/storage 
+chmod -R 770 /var/www/html/app/bootstrap/cache
 /usr/sbin/httpd -DFOREGROUND
