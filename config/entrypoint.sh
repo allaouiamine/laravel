8b911cd38ea42c3a1ae33ccb16d3c577106f5499
Mohamed Amine ALLAOUI <allaoui.amine@gmail.com>
2017-02-16 11:02:14 +0000

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -4,9 +4,9 @@ if [ -f /var/www/html/app/composer.json ]
 then
   cd app 
   composer update
+  chmod -R 770 /var/www/html/app/storage 
+  chmod -R 770 /var/www/html/app/bootstrap/cache
 fi
 chown -R apache:apache /var/www/html 
 chmod -R 550 /var/www/html/ 
-chmod -R 770 /var/www/html/app/storage 
-chmod -R 770 /var/www/html/app/bootstrap/cache
 /usr/sbin/httpd -DFOREGROUND
