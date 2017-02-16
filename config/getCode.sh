06c7ae3856adb0cbfe520cdfa8ec4a4bc91d282d
Mohamed Amine ALLAOUI <allaoui.amine@gmail.com>
2017-02-16 12:51:45 +0000

    Delete getCode.sh

--- config/getCode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone https://github.com/allaouiamine/app-cabinet-srv.git
-mv app-cabinet-srv app
-cd app 
-composer update
-chmod -R 770 /var/www/html/app/storage 
-chmod -R 770 /var/www/html/app/bootstrap/cache
